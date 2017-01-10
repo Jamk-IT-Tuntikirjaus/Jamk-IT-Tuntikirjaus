2904a83d1301201a88a3f33f19c85d1c99a2a3b2
Juha-Matti <juhamk91@gmail.com>
2017-01-10 18:41:06 +0200

    Sisältää toimivan componentin lisäyksen componentin sisään. 	Kirjoittaa siis html html koodiin. Sisältää toimivan accordion testin ag2-bootstrap moduulista. 	Tuo accordion objektin html kieleen. Ohje näiden tekemiseen tulee wikiin Angular2 things sivulle.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,5 +11,5 @@ export const ROUTES: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'detail', loadChildren: './+detail#DetailModule'},
   { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
-  { path: '**',    component: NoContentComponent },
+  { path: '**',    component: NoContentComponent }
 ];
