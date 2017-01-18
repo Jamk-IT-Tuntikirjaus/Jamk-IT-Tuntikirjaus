2d2c43f733d4c2a3cc3f2329a4ffc97c62e3efe1
Juha-Matti Kokkonen <juhamk91@gmail.com>
2017-01-18 12:32:38 +0200

    Login submit toimii paremmin

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,6 +1,7 @@
 
 
     import { Component } from '@angular/core';//, ViewChild, ElementRef, Renderer } from '@angular/core';
+    //declare var JQuert:any;
 
     @Component({
       selector: 'login',
@@ -17,12 +18,19 @@
         }
 
       }
-      onSubmit() {
+      onSubmit(username: string, password: string) {
+        this.data.username = username;
+        this.data.password = password;
+        alert("username: " + this.data.username);
+        alert("password: " + this.data.password);
+        //$('#staticModal').modal('hide');
+        //JQuery("#staticModal").modal("hide");
         //this.http.post("http://google.fi/", JSON.stringify(this.data)).subscribe();
         //loginmodal.hide();
         //this.renderer.invokeElementMethod(this.loginmodal.hide());
         //this.login.nativeElement.hide();
         //this.renderer.setElementStyle(this.login.nativeElement, "display", "none");
+
       }
 
     }
