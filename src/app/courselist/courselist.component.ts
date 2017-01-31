3c269c5f54962ddcb62032359d634bede6484f1c
ScoutPanda <eetu94@live.fi>
2017-01-31 16:55:26 +0200

    courselist should work dynamically

--- src/app/courselist/courselist.component.ts
+++ src/app/courselist/courselist.component.ts
@@ -1,48 +1,89 @@
 import {
-  Component
+    Component
 } from '@angular/core';
 
 console.log('`Courselist` component loaded asynchronously');
 
 @Component({
-  selector: 'courselist',
-  templateUrl: 'courselist.component.html',
-  styleUrls: ['courselist.component.css'],
+    selector: 'courselist',
+    templateUrl: 'courselist.component.html',
+    styleUrls: ['courselist.component.css'],
 })
 export class CourselistComponent {
-name="Courselist Component"
-public max: number = 750;
-public type: string;
-public teachinghours: number = 500;
-public planninghours: number = 350;
-public overworktime: number = 0;
-public stacked: any[] = [];
-public constructor() {
-  this.randomStacked();
-}
-public randomStacked(): void {
-  let types = ['success', 'info', 'danger'];
+    name = "Courselist Component"
+    private iteration: number = 0;
+    public type: string;
+    public stacked: any[] = [];
+    public koira: any[] = [];
+    public courses: any[] = [];
+    public constructor() {
+        this.mokkidata();
+        this.stackCourses();
+    }
 
-  this.stacked = [];
-  let total = 0;
-  let n = 3;
-  if (this.teachinghours + this.planninghours > this.max){
-      this.overworktime = (this.teachinghours + this.planninghours) - this.max;
-      this.max = this.overworktime*2 + this.max;
-  }
-  let value = [this.teachinghours / this.max * 100, this.planninghours / this.max * 100, this.overworktime / this.max * 100];
-  let hvalue = [this.teachinghours, this.planninghours, this.overworktime];
-  for (let i = 0; i < n; i++) {
-    total += value[i];
-    let barvalue = value[i];
-    let hourvalue = hvalue[i];
-    let type = types[i];
-    this.stacked.push({
-      hourvalue,
-      barvalue,
-      max: barvalue, // i !== (n - 1) ? value : 100,
-      type: type
-    });
-  }
-}
+    public stackCourses(): void {
+        let types = ['success', 'info', 'danger'];
+
+        let total = 0;
+        let n = 3;
+        for (let i = 0; i < this.courses.length; i++){
+
+            this.stacked = [];
+            if (this.courses[i].teachinghours + this.courses[i].planninghours > this.courses[i].max) {
+                this.courses[i].overworktime = (this.courses[i].teachinghours + this.courses[i].planninghours) - this.courses[i].max;
+                this.courses[i].max = this.courses[i].overworktime * 2 + this.courses[i].max;
+            }
+            let value = [this.courses[i].teachinghours / this.courses[i].max * 100, this.courses[i].planninghours / this.courses[i].max * 100, this.courses[i].overworktime / this.courses[i].max * 100];
+            let hvalue = [this.courses[i].teachinghours, this.courses[i].planninghours, this.courses[i].overworktime];
+            for (let j = 0; j < n; j++) {
+                total += value[j];
+                let barvalue = value[j];
+                let hourvalue = hvalue[j];
+                let type = types[j];
+                console.log("barvalue" + j + " " + barvalue);
+                console.log("hourvalue" + j + " " + hourvalue);
+                this.stacked.push({
+                    hourvalue,
+                    barvalue,
+                    type
+                });
+            }
+            this.koira.push(this.stacked);
+        }
+    }
+    private mokkidata(): void {
+        this.courses = [];
+        this.courses.push({
+            courseid: "IIO12110.6S0V1",
+            coursename: "Ohjelmistotuotannon käytännöt",
+            teachinghours: 700,
+            planninghours: 100,
+            overworktime: 0,
+            max: 750,
+        },
+        {
+            courseid: "ZZPP0500.7K0V7",
+            coursename: "Osaajana kehittyminen",
+            teachinghours: 300,
+            planninghours: 500,
+            overworktime: 0,
+            max: 500
+        },
+        {
+            courseid: "IIO13100.7K0V1",
+            coursename: "JavaEE-ohjelmointi",
+            teachinghours: 400,
+            planninghours: 700,
+            overworktime: 0,
+            max: 1000
+        },
+        {
+            courseid: "IIO13100.7K0V1",
+            coursename: "JavaEE-ohjelmointi",
+            teachinghours: 400,
+            planninghours: 700,
+            overworktime: 0,
+            max: 1000
+        });
+    }
 }
