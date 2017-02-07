4309cc849c067601fdb58eed1c84f3c45a8565bd
ScoutPanda <eetu94@live.fi>
2017-02-07 15:23:49 +0200

    courselist almost complete

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import {
-  inject,
-  async,
-  TestBed,
-  ComponentFixture
+    inject,
+    async,
+    TestBed,
+    ComponentFixture
 } from '@angular/core/testing';
 
 // Load the implementations that should be tested
@@ -12,45 +12,45 @@ import { AppState } from './app.service';
 import { MaterialModule } from '@angular/material';
 
 describe(`App`, () => {
-  let comp: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
+    let comp: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
 
-  // async beforeEach
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
-      imports: [MaterialModule.forRoot()],
-      schemas: [NO_ERRORS_SCHEMA],
-      providers: [AppState]
-    })
-    .compileComponents(); // compile template and css
-  }));
+    // async beforeEach
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent],
+            imports: [MaterialModule.forRoot()],
+            schemas: [NO_ERRORS_SCHEMA],
+            providers: [AppState]
+        })
+            .compileComponents(); // compile template and css
+    }));
 
-  // synchronous beforeEach
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp    = fixture.componentInstance;
+    // synchronous beforeEach
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
 
-    fixture.detectChanges(); // trigger initial data binding
-  });
+        fixture.detectChanges(); // trigger initial data binding
+    });
 
-  it(`should be readly initialized`, () => {
-    expect(fixture).toBeDefined();
-    expect(comp).toBeDefined();
-  });
+    it(`should be readly initialized`, () => {
+        expect(fixture).toBeDefined();
+        expect(comp).toBeDefined();
+    });
 
-  it(`should be @AngularClass`, () => {
-    expect(comp.url).toEqual('https://twitter.com/AngularClass');
-    expect(comp.angularclassLogo).toEqual('assets/img/angularclass-avatar.png');
-    expect(comp.name).toEqual('Angular 2 Webpack Starter');
-  });
+    it(`should be @AngularClass`, () => {
+        expect(comp.url).toEqual('https://twitter.com/AngularClass');
+        expect(comp.angularclassLogo).toEqual('assets/img/angularclass-avatar.png');
+        expect(comp.name).toEqual('Angular 2 Webpack Starter');
+    });
 
-  it('should log ngOnInit', () => {
-    spyOn(console, 'log');
-    expect(console.log).not.toHaveBeenCalled();
+    it('should log ngOnInit', () => {
+        spyOn(console, 'log');
+        expect(console.log).not.toHaveBeenCalled();
 
-    comp.ngOnInit();
-    expect(console.log).toHaveBeenCalled();
-  });
+        comp.ngOnInit();
+        expect(console.log).toHaveBeenCalled();
+    });
 
 });
--- src/app/courselist/courselistmodal/courselistmodal.component.ts
+++ src/app/courselist/courselistmodal/courselistmodal.component.ts
@@ -1,20 +1,21 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'courselistmodal',
-  templateUrl: './courselistmodal.component.html',
-  styleUrls: ['courselistmodal.component.css'],
+    selector: 'courselistmodal',
+    templateUrl: './courselistmodal.component.html',
+    styleUrls: ['courselistmodal.component.css'],
 })
 export class CourselistModalComponent {
     @Input() courseid: number;
+    @Input() coursename: string;
     @Output() ondelete = new EventEmitter();
     constructor() {
 
     }
     public deleteCourse(): void {
         try {
-        this.ondelete.emit(this.courseid)
-    }
-    catch(e) { console.error(e) }
+            this.ondelete.emit(this.courseid)
+        }
+        catch (e) { console.error(e) }
     }
 }
